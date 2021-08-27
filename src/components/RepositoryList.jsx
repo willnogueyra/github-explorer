5de4f1291e41d98926face1b6302a27454d692d5
William Nogueira <will.ribeiro@live.com>
2021-08-27 11:38:14 -0300

    Chamadas HTTP - Utilizando o useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss'
 
@@ -8,6 +9,14 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos') //buscar
+      .then(response => response.json()) // quando devolver a resposta, converte em json
+      .then(data => setRepositories(data)) // quando dados já convertidos, declarar na variavel
+  }, [])
+  
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
